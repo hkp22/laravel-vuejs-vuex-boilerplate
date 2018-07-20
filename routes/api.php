87e43ea9a2cc54b55851f0a7b3a9d22eff129afb
Harish Kumar <harish.pra22@gmail.com>
2018-07-20 14:05:47 +0530

    timeline controller

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ Route::post('/register', 'Auth\AuthController@register');
 Route::post('/login', 'Auth\AuthController@login');
 Route::post('/logout', 'Auth\AuthController@logout');
 
-Route::group(['middleware' => 'jwt.auth'], function() {
+Route::group(['middleware' => 'jwt.auth'], function () {
     Route::get('/profile', 'Auth\AuthController@user');
+    Route::get('/timeline', 'TimelineController@index');
 });
-
