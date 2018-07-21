67dc192ed0c3f347fd5b81d2792801e479af3f14
Harish Kumar <harish.pra22@gmail.com>
2018-07-21 14:23:28 +0530

    Vuejs Protecting routes

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -26,12 +26,17 @@ window.Vue = require("vue");
 Vue.component("app", require("./components/App.vue"));
 Vue.component("navigation", require("./components/Navigation.vue"));
 
-store.dispatch("auth/setToken").then(() => {
-  store.dispatch("auth/fetchUser").catch(() => {
+store
+  .dispatch("auth/setToken")
+  .then(() => {
+    store.dispatch("auth/fetchUser").catch(() => {
+      store.dispatch("auth/clearAuth");
+      router.replace({ name: "login" });
+    });
+  })
+  .catch(() => {
     store.dispatch("auth/clearAuth");
-    router.replace({ name: "login" });
   });
-});
 
 const app = new Vue({
   el: "#app",
--- resources/assets/js/routes/index.js
+++ resources/assets/js/routes/index.js
@@ -1,13 +1,15 @@
 import Vue from "vue";
-import Router from 'vue-router';
+import Router from "vue-router";
 // import routes from '../app/routes';
-import {routes as routes} from '../app/index';
-
+import { routes } from "../app/index";
+import beforeEach from "./beforeEach";
 
 Vue.use(Router);
 
 const router = new Router({
-    routes: routes
+  routes: routes
 });
+
+router.beforeEach(beforeEach);
 
 export default router;
