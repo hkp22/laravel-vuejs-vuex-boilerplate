f7ce61b547c355e8223019eb9e3e10769bfe1316
Harish Kumar <harish.pra22@gmail.com>
2018-07-21 15:16:08 +0530

    vuejs 404 handler

--- resources/assets/js/app/routes.js
+++ resources/assets/js/app/routes.js
@@ -1,5 +1,6 @@
-import home from './home/routes';
-import auth from './auth/routes';
-import timeline from './timeline/routes';
+import home from "./home/routes";
+import auth from "./auth/routes";
+import timeline from "./timeline/routes";
+import errors from "./errors/routes";
 
-export default [...home, ...auth, ...timeline];
+export default [...home, ...auth, ...timeline, ...errors];
